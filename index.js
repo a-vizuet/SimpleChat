d9a5b66d6a1a28e97fdbb1988ca4a8357a524f0c
Angel Vizuet <megustalafantabienfria@hotmail.com>
2017-10-18 20:57:21 -0500

    no xss, sí mensajes privado, mejor código y funcionalidades

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var port = process.env.port || 8080;
 var users = [];
-var testing;
 
 function deleteUser(userToDelete){
 	let index = users.findIndex((user) => {
@@ -19,27 +18,23 @@ app.get('/', (req, res) => {
 	res.sendFile(__dirname+'/public/index.html');
 });
 
-app.get('/:username', (req, res) => {
-	if(testing.username != undefined) res.sendFile(__dirname+'/public/chat.html');
-	else res.send({message: 'No tienes permitido ésto.'});
-});
-
 io.on('connection', (socket) => {
 	
-	// public chat
 	socket.on('add user', (user) => {
 		socket.username = user.username;
 		users.push(user);
 		io.emit('this.connection', {username: 'Bot', message: 'Se ha conectado el usuario '+socket.username});
 		console.log('Se ha conectado el usuario '+socket.username);
 		io.emit('get users', users);
-
-		testing = socket;
 	});
 
 	socket.on('chat message', (msg) => {
 		io.emit('chat message', {username: socket.username, message: msg});
 	});
+
+	socket.on('private message', (msg) => {
+		socket.broadcast.to(msg.idTo, msg.from).emit('private message', msg);
+	})
 
 	socket.on('disconnect', () => {
 		if(socket.username != undefined){
@@ -50,14 +45,9 @@ io.on('connection', (socket) => {
 		}
 	});
 
-	// private chat
-	socket.on('private message', (data) => {
-		socket.broadcast.to(data.to).emit('private message', {message: 'k'});
-	})
-
 });
 
 server.listen(port, (err) => {
 	if(err) console.error("Ha habido un problema al iniciar el servidor :( Error: "+err);
-	else console.info("Se ha iniciado el server en http://localhost:"+port+"!!");	
+	else console.info("Se ha iniciado el server en el puerto "+port+"!!");	
 })
--- public/main.js
+++ public/main.js
@@ -1,56 +1,148 @@
 var socket = io();
+var xss = /[<].+[>]+(...)+[<&frasl].+[>]|[<].+[>]+[<&frasl].+[>]/i;
 
 function addUser(){
-	let trimuser = $('#username').val().trim();
+	let user = $('#username').val();
 	$('#alert1').hide();
 	
-	if(trimuser){
-		let username = $('#username').val();
-		$('.login').hide();
-		$('.chat').show();
-		socket.username = username;
-		socket.emit('add user', {id: socket.id, username: username});
-	}else $('#alert1').show();
+	if(xss.test(user)){
+		$('#alert1').show();
+		$('#username').val("");
+	}else{
+		let usertrim = $('#username').val().trim();
+
+		if(usertrim){
+			let username = $('#username').val();
+			$('.login').hide();
+			$('.chat').show();
+			socket.username = username;
+			socket.emit('add user', {id: socket.id, username: username});
+		}else{
+			$('#alert1').show();
+			$('#username').val("");	
+		}
+	}	
 }
 
 function addMessage(){
-	let trimmessage = $('#text').val().trim();
+	let message = $('#text').val(); 
 	$('#alert2').hide();
 
-	if(trimmessage){
-		let message = $('#text').val();
-		let messagehtml = `<div class="message alert myalert"><strong>Tú</strong> dices: <p class="pmessage">`+message+`</p></div>`;
-		socket.emit('chat message', message);
-	}else $('#alert2').show();
+	if(xss.test(message)){
+		$('#alert2').show();
+		$('#text').val("");
+	}else{
+		let trimmessage = $('#text').val().trim();
+
+		if(trimmessage){
+			socket.emit('chat message', message);
+			$('#text').val("");
+		}else{ 
+			$('#alert2').show();
+			$('#text').val("");
+		}
+	}
 }
 
+function addUserKey(){
+	var once=0;
+	$(document).keyup((e) => { 
+		if(e.keyCode == 13 && once == 0) 
+			addUser();
+			once = 1;
+	});
+}
+
+function addMessageKey(){
+	var once=0;
+	$(document).keyup((e) => { 
+		if(e.keyCode == 13 && once == 0) 
+			addMessage();
+			once = 1;
+	});
+}
+
+function privateMessageChat(v1, v2){
+	$('#generalMessage').hide();
+	$('#privateMessage').show();
+	$('#idPrivate').val(v1);
+	$('#usernamePrivate').val(v2);
+}
+
+function addPrivateMessage(){
+	let message = $('#textPrivate').val(); 
+	let idTo = $('#idPrivate').val();
+	let usernameTo = $('#usernamePrivate').val();
+	$('#alert3').hide();
+
+	if(xss.test(message)){
+		$('#alert3').show();
+		$('#textPrivate').val("");
+	}else{
+		let trimmessage = $('#textPrivate').val().trim();
+
+		if(trimmessage){
+			let messagehtml = `<div class="message alert alert-warning"><strong>Tú</strong> le has enviado un mensaje privado a <strong>${usernameTo}</strong>: <p class="pmessage">${message}</p></div>`;
+			socket.emit('private message',{idTo: idTo, usernameTo: usernameTo, from: socket.username, message: message});
+			document.querySelector('#messages').scrollTop = document.querySelector('#messages').scrollHeight;
+			$('#messages').append(messagehtml);
+			$('#textPrivate').val("");
+			$('#generalMessage').show();
+			$('#privateMessage').hide();
+			$('#idPrivate').val("");
+			$('#usernamePrivate').val("");
+		}else{ 
+			$('#alert3').show();
+			$('#textPrivate').val("");
+		}
+	}
+}
+
+function addPrivateMessageKey(){
+	var once=0;
+	$(document).keyup((e) => { 
+		if(e.keyCode == 13 && once == 0) 
+			addPrivateMessage();
+			once = 1;
+	});
+}
+
+
 socket.on('this.connection', (connected) => {
-	let connectedhtml = `<div class="message alert alert-info"><strong>`+connected.username+`</strong> dice: <p class="pmessage">`+connected.message+`</p></div>`;
+	let connectedhtml = `<div class="message alert alert-info"><strong>${connected.username}</strong> dice: <p class="pmessage">${connected.message}</p></div>`;
 	$('#messages').append(connectedhtml);
 });
 
 socket.on('chat message', (msg) => {
 	if(socket.username == msg.username){
-		let messagehtml = `<div class="message alert myalert"><strong>Tú</strong> dices: <p class="pmessage">`+msg.message+`</p></div>`;
+		let messagehtml = `<div class="message alert myalert"><strong>Tú</strong> dices: <p class="pmessage">${msg.message}</p></div>`;
 		$('#messages').append(messagehtml);
 	}
 	else{
-		let messagehtml = `<div class="message alert alert-success"><strong>`+msg.username+`</strong> dice: <p class="pmessage">`+msg.message+`</p></div>`;
+		let messagehtml = `<div class="message alert alert-success"><strong>${msg.username}</strong> dice: <p class="pmessage">${msg.message}</p></div>`;
 		$('#messages').append(messagehtml);
 	}
+	document.querySelector('#messages').scrollTop = document.querySelector('#messages').scrollHeight;
+});
+
+socket.on('private message', (msg) => {
+	let messagehtml = `<div class="message alert alert-warning"><strong>${msg.from}</strong> te envió un mensaje privado: <p class="pmessage">${msg.message}</p></div>`;
+	$('#messages').append(messagehtml);
+
+	document.querySelector('#messages').scrollTop = document.querySelector('#messages').scrollHeight;
 });
 
 socket.on('get users', (users) => {
 	let usershtml = "";
 
 	users.forEach((user) => {
-		if(user.username == socket.username) usershtml += `<li class="list-group-item active"><a>`+user.username+`</a></li>`;
-		else usershtml += `<li class="list-group-item"><a href="`+window.location+user.username+`?id=`+user.id+`?myid=`+socket.id+`" target="_blank">`+user.username+`</a></li>`;
+		if(user.username == socket.username) usershtml += `<input type="button" value="${user.username}" class="list-group-item list-group-item-action active disabled">`;
+		else usershtml += `<input type="button" value="${user.username}" class="list-group-item list-group-item-action" onclick="privateMessageChat('${user.id}', '${user.username}')">`;
 	});
 	document.querySelector('#user-list').innerHTML = usershtml;
 });
 
 socket.on('disconnect', (disconnected, users) => {
-	let disconnectedhtml = `<div class="message alert alert-info"><strong>`+disconnected.username+`</strong> dice: <p class="pmessage">`+disconnected.message+`</p></div>`;
+	let disconnectedhtml = `<div class="message alert alert-info"><strong>${disconnected.username}</strong> dice: <p class="pmessage">${disconnected.message}</p></div>`;
 	$('#messages').append(disconnectedhtml);
 });
